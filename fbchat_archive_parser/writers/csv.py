0f9e2f9477b2e91bacd25ad74b3d555effbf76e2
Dillon Dixon <dillondixon@gmail.com>
2016-01-21 17:41:09 -0800

    Fixed issues in csv writer and corrected json writer path

--- fbchat_archive_parser/writers/csv.py
+++ fbchat_archive_parser/writers/csv.py
@@ -1,4 +1,5 @@
-from __future__ import unicode_literals
+from __future__ import unicode_literals, absolute_import
+
 from .writer import Writer
 
 import sys
@@ -14,10 +15,11 @@ class CsvWriter(Writer):
     def get_writer(self, stream, include_id=False):
 
         columns = [SENDER_KEY, DATE_KEY, MESSAGE_KEY]
-        
+
         if include_id:
             columns = [THREAD_ID_KEY] + columns
 
+        import pdb; pdb.set_trace()
         w = csv.DictWriter(stream,
             fieldnames=columns,
             quoting = csv.QUOTE_MINIMAL,
@@ -28,25 +30,25 @@ class CsvWriter(Writer):
 
     def write_history(self, history, stream=sys.stdout, writer=None):
         if not writer:
-            writer = self.get_writer(True)
+            writer = self.get_writer(stream, True)
         for _, thread in history.iteritems():
-            self.write_thread(thread, writer)
+            self.write_thread(thread, writer=writer)
 
     def write_thread(self, thread, stream=sys.stdout, writer=None):
         if not writer:
-            writer = self.get_writer(True)
+            writer = self.get_writer(stream, True)
         for message in thread.messages:
-            self.write_message(message, writer)
+            self.write_message(message, thread, writer=writer)
 
     def write_message(self, message, parent=None, stream=sys.stdout, writer=None):
         if not writer:
-            writer = self.get_writer(True)
+            writer = self.get_writer(stream, True)
         row = {
             SENDER_KEY: message.sender,
             DATE_KEY: message.timestamp.strftime(self.DATE_DOC_FORMAT),
             MESSAGE_KEY: message.content
-        }]
+        }
         if parent:
-            row[THREAD_ID_KEY] = ", ".join(parent.participants),
+            row[THREAD_ID_KEY] = "<unknown>" if not parent else ", ".join(parent.participants)
 
         writer.writerow(row)
