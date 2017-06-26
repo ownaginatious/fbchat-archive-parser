f97a27a138829665e2fb0911641cb40e6b137d44
SouthPeter98 <pedro_98_almeida@hotmail.com>
2017-06-26 10:29:19 +0100

    Corrected Formating errors
    tabs to spaces

--- tests/test_timestamps.py
+++ tests/test_timestamps.py
@@ -84,7 +84,7 @@ class TestTimestamps(unittest.TestCase):
         timestamp_raw = "4 de dezembro de 2016 às 13:54 UTC-07"
         self.run_timestamp_test(timestamp_raw)
 		
-	def test_portuguese_portugal(self):
+    def test_portuguese_portugal(self):
         timestamp_raw = "Domingo, 4 de Dezembro de 2016 às 13:54 UTC-07"
         self.run_timestamp_test(timestamp_raw)
 
